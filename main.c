904b13023d588a6bfe8bc44aab8316c0f2f6e0bc
deantosh <deantoshdaiddoh@gmail.com>
2023-08-24 10:48:17 -0700

    file updated

--- main.c
+++ main.c
@@ -4,6 +4,20 @@
  */
 
 #include "monty.h"
+global_t monty_vars;
+
+/**
+ * init_global_vars - initializes the global variables.
+ * @stream: a pointer to monty file.
+ * Return: void.
+ */
+void init_global_vars(FILE *stream)
+{
+	monty_vars.head = NULL;
+	monty_vars.stream = stream;
+	monty_vars.current_line = 1;
+	monty_vars.opcode_value = NULL;
+}
 
 /**
  * main - the program entry point.
@@ -15,12 +29,52 @@
 int main(int argc, char **argv)
 {
 	void (*f)(stack_t **stack, unsigned int line_number);
-	char *file_name, *line = NULL;
-	char *line[2] = {NULL, NULL};
+	char *line = NULL, *line_data[2] = {NULL, NULL};
 	FILE *stream;
 	size_t len = 0;
 	ssize_t nread;
-	int line_number = 1;
+
+	/*check program usage / if file is present*/
+	stream = check_usage_file(argc, argv);
+
+	/*initialize global monty variables*/
+	init_global_vars(stream);
+
+	/*read file*/
+	while ((nread = getline(&line, &len, stream)) != -1)
+	{
+		line_data[0] = strtok(line, " \t\n"); /*name of opcode*/
+		f = select_opcode_func(line_data[0]);
+		if (f == NULL)
+		{
+			fprintf(stderr, "L%u: ", monty_vars.current_line);
+			fprintf(stderr, "unknown instruction %s\n", line_data[0]);
+			exit(EXIT_FAILURE);
+		}
+		/*execute instructions*/
+		monty_vars.opcode_value = strtok(NULL, " \t\n");
+		f(&monty_vars.head, monty_vars.current_line);
+		monty_vars.current_line++;
+	}
+	free(line);
+	/*close the file*/
+	fclose(stream);
+	return (0);
+}
+
+
+/**
+ * check_usage_file - checks for program usage and if monty file
+ *			is provided.
+ * @argc: the number of args.
+ * @argv: the list of args.
+ *
+ * Return: a pointer to the opened file.
+ */
+FILE *check_usage_file(int argc, char **argv)
+{
+	FILE *stream;
+	char *file_name;
 
 	/*if user does not provide the file*/
 	if (argc != 2)
@@ -28,7 +82,9 @@ int main(int argc, char **argv)
 		printf("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+
 	file_name = argv[1];
+
 	/*open file*/
 	stream = fopen(file_name, "r");
 	if (stream == NULL) /*can't open*/
@@ -36,22 +92,5 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Error: Can't open file %s\n", file_name);
 		exit(EXIT_FAILURE);
 	}
-	/*read file*/
-	while ((nread = getline(&line, &len, stream)) != -1)
-	{
-		line[0] = strtok(line, " \t\n"); /*name of opcode*/
-		f = select_opcode_func(line[0]);
-		if (f == NULL)
-		{
-			fprintf(stderr, "L%u: unknown instruction %s\n", line_num, line[0]);
-			exit(EXIT_FAILURE);
-		}
-		printf("function obtained\n");
-		return (0);
-		line_number++;
-	}
-	free(line);
-	/*close the file*/
-	fclose(stream);
-	return (0);
+	return (stream);
 }
--- monty.h
+++ monty.h
@@ -10,21 +10,6 @@
 #include <sys/types.h>
 
 /**
- * struct globals - global variables to store values used during instructions
- *			execution.
- * @current_line: the current line.
- * @opcode_value: the second parameter on current line.
- *
- * Description: a doubly linked list node structure.
- * for stack, queue, lifo and fifo.
- */
-typedef struct globals
-{
-	char *current_line;
-	char *opcode_value;
-} global_t;
-
-/**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
@@ -41,6 +26,25 @@ typedef struct stack_s
 } stack_t;
 
 /**
+ * struct globals - global variables to store values used during instructions
+ *			execution.
+ * @head: a pointer to doubly linked list.
+ * @stream: a pointer to monty file.
+ * @current_line: the current line.
+ * @opcode_value: the second parameter on current line.
+ *
+ * Description: a doubly linked list node structure.
+ * for stack, queue, lifo and fifo.
+ */
+typedef struct globals
+{
+	stack_t *head;
+	FILE *stream;
+	unsigned int current_line;
+	char *opcode_value;
+} global_t;
+
+/**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
@@ -55,6 +59,10 @@ typedef struct instruction_s
 } instruction_t;
 
 /*FUNCTIONS*/
+
+/*file: main.c*/
+void init_global_vars();
+FILE *check_usage_file(int argc, char **argv);
 
 /*file: execute_instructions*/
 void (*select_opcode_func(char *opcode_name))(stack_t **stack, unsigned int line_number);
