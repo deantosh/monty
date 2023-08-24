e17b35045cbc4b72fedd7b520f827ea387a90167
deantosh <deantoshdaiddoh@gmail.com>
2023-08-24 02:02:46 -0700

    selects the right function to execute

---
+++ get_opcode_func.c
@@ -0,0 +1,30 @@
+/*
+ * Author: Deantosh Daiddoh & Lucky Archibong
+ * File: get_opcode_func.c
+ */
+
+#include "monty.h"
+
+/**
+ * select_opcode_func - selects the opcode that matches the name parsed.
+ * @op: the opcode name.
+ *
+ * Return: the function to execute the opcode.
+ */
+void (*select_opcode_func(char *op))(stack_t **stack, unsigned int line_number)
+{
+	int i = 0;
+
+	/*create a list of opcodes*/
+	instruction_t opcode_list[] = {
+		{NULL, NULL}
+	};
+
+	while (opcode_list[i].opcode)
+	{
+		if (strcmp(op, opcode_list[i].opcode) == 0)
+			return (opcode_list[i].f);
+		i++;
+	}
+	return (NULL);
+}
