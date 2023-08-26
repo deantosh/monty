d05bfb84675cac26fec1d70d7ddc0e746db16051
deantosh <deantoshdaiddoh@gmail.com>
2023-08-26 15:01:51 -0700

    task 8: mul opcode instruction added

--- instructions_2.c
+++ instructions_2.c
@@ -153,7 +153,7 @@ void _mul(stack_t **stack, unsigned int line_number)
 	/*if elements are less than two*/
 	if (one == NULL || one->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
 		free_monty_vars();
 		exit(EXIT_FAILURE);
 	}
